958cf01c36f9f580178ad370ed4ab4825ff85567
wangyuanyuan <yuanyuan.wang18@Pactera.com>
2020-11-07 10:56:53 +0800

    aa

---
+++ precache-manifest.35e8b9dd45961c756bc80f027f0f81a7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c74337234bf11be3cb1a",
+    "url": "/css/app.17d0086d.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "28addf84f9e0547084fdeff2976128d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7832d826f67a60dda82",
+    "url": "/js/about.750ac6a7.js"
+  },
+  {
+    "revision": "c74337234bf11be3cb1a",
+    "url": "/js/app.f9ac01b6.js"
+  },
+  {
+    "revision": "c9dfe75b80a9ced4af60",
+    "url": "/js/chunk-vendors.6bcfce0a.js"
+  },
+  {
+    "revision": "3b1c77ef6641b4a067b6241152036521",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
